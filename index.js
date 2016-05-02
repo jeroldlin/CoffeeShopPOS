54e7815e903c8a036e32cbd332f355ea5edee2b6
Jerry Lin <jeroldlin@gmail.com>
2016-05-02 09:55:41 -0500

    for Heroku

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/chat_example_html/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(path.join(__dirname, 'lib')));
 
 io.on('connection', function(socket){
   console.log('a user connected');
