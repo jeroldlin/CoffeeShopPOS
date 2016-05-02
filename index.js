d1eb406ad1b8c5db619b42379c6b6c9c563d2f5a
Jerry Lin <jeroldlin@gmail.com>
2016-05-02 10:01:36 -0500

    For avoidong Heroku $PORT error

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get('/', function(req, res){
 
 app.use(express.static(path.join(__dirname, 'lib')));
 
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
